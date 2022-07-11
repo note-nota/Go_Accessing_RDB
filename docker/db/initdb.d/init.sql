3b353e06648864405bc4e122422f49dd640e1792
Naoki Ohta <n.ohta@meleap.com>
2022-07-11 11:53:56 +0900

    commit project.

---
+++ docker/db/initdb.d/init.sql
@@ -0,0 +1,35 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema recordings
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `recordings` ;
+
+-- -----------------------------------------------------
+-- Schema recordings
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `recordings` DEFAULT CHARACTER SET utf8 ;
+USE `recordings` ;
+
+-- -----------------------------------------------------
+-- Table `recordings`.`album`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `recordings`.`album` ;
+
+CREATE TABLE `recordings`.`album` (
+  id         INT AUTO_INCREMENT NOT NULL,
+  title      VARCHAR(128) NOT NULL,
+  artist     VARCHAR(255) NOT NULL,
+  price      DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO album
+  (title, artist, price)
+VALUES
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
